641566e2582a553b9a07ac71f8c2463c65066945
gayov_ra <gayov_ra@ekb.sima-land.ru>
2019-08-19 18:02:21 +0500

    Вывод меню подсказок

--- src/Components/input/index.js
+++ src/Components/input/index.js
@@ -5,6 +5,7 @@ const Input = ({
   value = '',
   items = [],
   onChange,
+  isFetching = false,
 }) => {
   return (
     <Autocomplete
@@ -15,10 +16,35 @@ const Input = ({
           {item.label}
         </div>
       )}
+      renderMenu={renderSuggestionsPopup(items, value, isFetching)}
       value={value}
       onChange={onChange}
       onSelect={(val) => value = val}
     />
+  );
+};
+
+/**
+ * Возвращает элемент попапа подсказок.
+ * @param {ReactElement[]} items Массив элементов подсказок.
+ * @param {string} value Введённое значение дял поиска.
+ * @param {boolean} isFetching Идет ли в данный момент загрузка.
+ * @returns {ReactElement} Элемент попапа подсказок.
+ */
+export const renderSuggestionsPopup = (items, value, isFetching) => {
+  // @todo использовать <Popup />
+  return (
+    <div className='b-popup'>
+      {Boolean(!items.length) && (
+        <span className='dadata-popup-tooltip'>
+          {/*{Boolean(isFetching) && (*/}
+          {/*  <Preloader size={SIZES.small} />*/}
+          {/*)}*/}
+          {Boolean(!isFetching && value) && 'Неизвестный адрес'}
+        </span>
+      )}
+      {items}
+    </div>
   );
 };
 
